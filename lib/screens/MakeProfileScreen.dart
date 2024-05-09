05b3c4aad2df9416175881d99a85314bac075baf
Muhammad Shahmir Yousaf <110347102+ShahmirYousaf@users.noreply.github.com>
2024-05-09 17:40:45 +0500

    Design Ipmroved

--- lib/screens/MakeProfileScreen.dart
+++ lib/screens/MakeProfileScreen.dart
@@ -19,6 +19,8 @@ class MakeProfileScreen extends StatefulWidget {
 
 class _MakeProfileScreenState extends State<MakeProfileScreen> {
   final _auth = FirebaseAuth.instance;
+  Color customColor = Color.fromRGBO(32, 61, 79, 1.0);
+  Color customColor2 = Color.fromRGBO(28, 183, 167, 1.0);
   final _firestore = FirebaseFirestore.instance;
   late User? loggedInUser;
   late String firstName = '';
@@ -81,111 +83,153 @@ class _MakeProfileScreenState extends State<MakeProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Create Profile'),
+        title: Text('Make Your Profile'),
       ),
-      body: ModalProgressHUD(
-        inAsyncCall: showSpinner,
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextField(
-                onChanged: (value) {
-                  firstName = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(labelText: 'First Name'),
-              ),
-              SizedBox(height: 8.0),
-              TextField(
-                onChanged: (value) {
-                  lastName = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(labelText: 'Last Name'),
-              ),
-              SizedBox(height: 8.0),
-              GestureDetector(
-                onTap: getImage,
-                child: Container(
-                  height: 150.0,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10.0),
+
+        body: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Stack (
+          children: [Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  onChanged: (value) {
+                    firstName = value;
+                  },
+                  textAlign: TextAlign.center,
+                  decoration: kTextFieldDecoration.copyWith(labelText: 'First Name', hintText: 'Enter your First Name',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: customColor), // Change the border color
+                      borderRadius: BorderRadius.circular(30.0), // Adjust border radius as needed
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: customColor), // Change the border color
+                      borderRadius: BorderRadius.circular(30.0), // Adjust border radius as needed
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: customColor), // Change the border color
+                      borderRadius: BorderRadius.circular(30.0), // Adjust border radius as needed
+                    )),
+                ),
+                SizedBox(height: 16.0),
+                TextField(
+                  onChanged: (value) {
+                    lastName = value;
+                  },
+                  textAlign: TextAlign.center,
+                  decoration: kTextFieldDecoration.copyWith(labelText: 'Last Name', hintText: 'Enter your Last Name',border: OutlineInputBorder(
+                    borderSide: BorderSide(color: customColor), // Change the border color
+                    borderRadius: BorderRadius.circular(30.0), // Adjust border radius as needed
                   ),
-                  child: _image != null
-                      ? kIsWeb
-                      ? Image.network(_image!.path)
-                      : Image.file(
-                    _image!,
-                    fit: BoxFit.cover,
-                  )
-                      : Icon(
-                    Icons.add_a_photo,
-                    size: 50.0,
-                    color: Colors.grey[400],
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: customColor), // Change the border color
+                        borderRadius: BorderRadius.circular(30.0), // Adjust border radius as needed
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: customColor), // Change the border color
+                        borderRadius: BorderRadius.circular(30.0), // Adjust border radius as needed
+                      )),
+                ),
+                SizedBox(height: 16.0),
+
+                GestureDetector(
+                  onTap: getImage,
+                  child: CircleAvatar(
+                    radius: 75.0,
+                    backgroundColor: customColor,
+                    backgroundImage: _image != null ? FileImage(_image!) : null,
+                    child: _image == null
+                        ? Icon(
+                      Icons.add_a_photo,
+                      size: 50.0,
+                      color: customColor2,
+                    )
+                        : null,
                   ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () async {
-                  setState(() {
-                    showSpinner = true;
-                  });
 
-                  if (firstName.isEmpty || lastName.isEmpty) {
-                    // Check if mandatory fields are empty
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text('Missing Information'),
-                          content: Text('Please provide both first name and last name.'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
+                SizedBox(height: 16.0),
+                ElevatedButton(
+
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+
+                    if (firstName.isEmpty || lastName.isEmpty) {
+                      // Check if mandatory fields are empty
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Missing Information'),
+                            content: Text('Please provide both first name and last name.'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                      setState(() {
+                        showSpinner = false;
+                      });
+                      return;
+                    }
+
+                    final imageUrl = await _uploadImage();
+                    if (imageUrl != null) {
+                      // Create a subcollection for user data within the user's document
+                      final userRef = _firestore.collection('users').doc(loggedInUser!.uid);
+                      final userInfoRef = userRef.collection('user_info');
+
+                      await userInfoRef.add({
+                        'first_name': firstName,
+                        'last_name': lastName,
+                        'profile_picture': imageUrl,
+                        'followers': followers,
+                        'following': following,
+                      });
+
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    } else {
+                      print('Image upload failed.');
+                    }
                     setState(() {
                       showSpinner = false;
                     });
-                    return;
-                  }
-
-                  final imageUrl = await _uploadImage();
-                  if (imageUrl != null) {
-                    // Create a subcollection for user data within the user's document
-                    final userRef = _firestore.collection('users').doc(loggedInUser!.uid);
-                    final userInfoRef = userRef.collection('user_info');
-
-                    await userInfoRef.add({
-                      'first_name': firstName,
-                      'last_name': lastName,
-                      'profile_picture': imageUrl,
-                      'followers': followers,
-                      'following': following,
-                    });
-
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  } else {
-                    print('Image upload failed.');
-                  }
-                  setState(() {
-                    showSpinner = false;
-                  });
-                },
-                child: Text('Create Profile'),
-              ),
-            ],
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(customColor), // Change button background color
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Change text color
+                  ),
+                  child: Text('Save'),
+                ),
+              ],
+            ),
           ),
+
+            Positioned(
+              top: 30.0, // Adjust the position as needed
+              right: 180.0,
+              child: Image.asset(
+                '../assets/InvenTale.png',
+                width: 120.0,
+                height: 120.0,
+              ),
+            ),
+        ],
         ),
       ),
+
     );
   }
 }
+
